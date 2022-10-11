1e1a0fab6f5671bd5760f84f97d2dcc10b47cb74
Erik Souza <erikjhow.ed@gmail.com>
2022-10-11 09:33:42 -0300

    second commit

--- index.php
+++ index.php
@@ -6,20 +6,39 @@
 </head>
 
 <body>
-  <div class='container'>
+  <div class="bloc">
+    <div class="img">
+      <img src="./imagns/office.svg" alt="office">
+    </div>
+    <div class='container'>
     <form action="valida_login.php"  method='post'>
       <div class='card'>
+      <h2>Login:</h2>
         <div>
-          E-mail<input type='email' name='email' placeholder='E-mail' />
+          <p>E-mail</p>
+          <input type='email' name='email' placeholder='E-mail' />
         </div>
         <div>
-          Senha<input type='password' name='senha' placeholder='Digite sua senha' />
+        <p>Senha</p>
+          <input type='password' name='senha' placeholder='Digite sua senha' />
         </div>
         <div>
           <button type="submit">Confirmar</button>
+          <div class="p">
+            <p>ou</p>
+          </div>
+            <div class="outros">
+              <img class="g" src="./imagns/google.svg" alt="">
+              <h3>Logar com o Google</h3>
+            </div>
+            <div class="outros">
+              <img class="git" src="./imagns/github.svg" alt="">
+              <h3>Logar com o GitHub</h3>
+            </div>          
         </div>
       </div>
     </form>
+  </div>
   </div>
 
   <?php
